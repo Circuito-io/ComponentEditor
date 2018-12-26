28fc02099cb56a49b8ee18823d03531b291f444e
Ohad <ohad@robo-plan.com>
2018-12-26 14:31:14 +0000

    add speaker 3w

---
+++ components/Coders/SpeakerStereo3W/SpeakerStereo3W.h
@@ -0,0 +1,12 @@
+#ifndef SpeakerStereo3W_H
+#define SpeakerStereo3W_H
+
+#include "Speaker.h"
+
+class SpeakerStereo3W : public Speaker  {
+  public:
+    SpeakerStereo3W(const int pin);
+};
+
+
+#endif //StereoSpeaker_H
