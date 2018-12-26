33c763af5444b8d924802ba1e6aaa5249636b2fa
Eyal <eyal@robo-plan.com>
2018-12-26 10:20:42 +0000

    added illustration

--- src/home.js
+++ src/home.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { Grid, Row, Col, Well , Button} from "react-bootstrap";
+import { Grid, Row, Col, Well, Button, Image } from "react-bootstrap";
 import { BlocksList } from "./blocks-list";
 import { NewComponent } from "./newcomponent";
 
 export class Home extends React.Component {
   render() {
-      return (
-        <Grid>
+    return (
+      <Grid>
           <Row>
             <Col xs={6}>
               <Well>
@@ -31,12 +31,14 @@ export class Home extends React.Component {
                 These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. See deployment for notes on how to deploy the project on a live system.  
                 <br/>
                 <br/>
+                <Image src="https://res.cloudinary.com/circuito/image/upload/v1545819424/Circuito_4_Entities.png" responsive />
+                <br/>
+                <br/>
                 <iframe width="560" height="315" src="https://www.youtube.com/embed/9YffrCViTVk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
               </Well>
             </Col>
           </Row>
         </Grid>
-      );
-    }
+    );
+  }
 }
-
