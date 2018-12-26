a2849bc9784be8cbccfc26c7256cabc8217723fb
Eyal <eyal@robo-plan.com>
2018-12-26 16:27:59 +0000

    add pintypes to svgcreator

--- src/svg-creator.js
+++ src/svg-creator.js
@@ -18,10 +18,10 @@ export class SVGCreator extends React.Component {
             show: false,
             resSVG: '',
             defaultSVG: {
-                name: "Demo",
-                width: 100,
+                name: "Demo Symbol",
+                width: 100, 
                 height: 150,
-                color: 'blue',
+                color: '#8A231E',
                 pins: [
                     { name: "GND", type: "male" },
                     { name: "VCC", type: "male" }
@@ -38,26 +38,45 @@ export class SVGCreator extends React.Component {
         var pins = ''
         if (formData.pins != null)
             pins = formData.pins.map((pin, index) => { 
-                var x = 10+7.2*index;
+                var spacing = 7.2;
+                var x = 10+spacing*index;
                 var y = formData.height-10;
+
+                var textx = x+spacing/2;
+                var texty = y-5;
+                
+                var pinShape =''
+                if (formData.pinTypes == "pads")
+                    pinShape = (
+                    <circle 
+                        cx={x}
+                        cy={y}
+                        data-cir-type={pin.type}
+                        id={"circuitoCon_" + pin.name}
+                        r="2.05" 
+                        style={{fill:'none', stroke:'#e8e5de', 'stroke-miterlimit':10}}
+                    />);
+                else
+                    pinShape = (
+                        <g transform={`translate(${x-4} ${y-4})`}>
+                            <polygon points="5.77 0 7.2 1.53 7.2 5.77 5.77 7.2 1.54 7.2 0 5.77 0 1.53 1.54 0 5.77 0"></polygon>
+                            <rect x="2.69" y="3.6" width="2.27" height="18.75"></rect>
+                            <rect id={"circuitoCon_" + pin.name} x="2.69" y="20.08" width="2.27" height="2.27"></rect>
+                        </g>
+                        );
+                
                 return (
                     <g>
                         <text
-                            x={x}
-                            y={y}
+                            x={textx}
+                            y={texty}
                             fill="white"
-                            transform={`rotate(-90,${x},${y})`}
+                            style={{'font-family':'monospace', 'font-size':10}}
+                            transform={`rotate(-90,${textx},${texty})`}
                         >
                             {pin.name}
                         </text>
-                        <circle 
-                            cx={x}
-                            cy={y}
-                            data-cir-type={pin.type}
-                            id={"circuitoCon_" + pin.name}
-                            r="2"
-                            style={{fill:'white'}}
-                        />
+                        {pinShape}
                     </g>
                 )}
                 )
@@ -65,9 +84,15 @@ export class SVGCreator extends React.Component {
         var res = (
             <svg width={formData.width} height={formData.height} xmlns="http://www.w3.org/2000/svg">
                 <title>{formData.name}</title>
-                
                 <rect width={formData.width} height={formData.height} style={{fill:formData.color}} />
-                <text x={5} y={10} fill="white">{formData.name}</text>
+                <text 
+                    x={5}
+                    y={10}
+                    fill="white"
+                    style={{'font-family':'monospace', 'font-size':10}}
+                >
+                    {formData.name}
+                </text>
                 
                 <g id="Connectors">
                     {pins}
@@ -87,7 +112,7 @@ export class SVGCreator extends React.Component {
     handleShow() {
         this.setState({ show: true });
     }
-
+    
     render() {
         return (
             <div>
