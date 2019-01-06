6104ad4be23f8183866b2b6732f4f867caee1c77
Ohad <ohad@robo-plan.com>
2019-01-06 11:03:10 +0000

    change circuit names 6/1

--- compUtils/6.1Mods.py
+++ compUtils/6.1Mods.py
@@ -8,7 +8,27 @@ blocksByName = {}
 filesToAdd = []
 
 
-# 1. change key: 'circuit' to 'circuits'
+# # 1. change key: 'circuit' to 'circuits'
+# for (dirpath, dirnames, filenames) in os.walk(path):
+    
+#     for filename in filenames:
+#         if filename.endswith('.json'): 
+            
+#             print(filename)
+
+#             json_data=open(dirpath + '/' + filename).read()
+#             data = json.loads(json_data)
+            
+            
+#             if 'circuit' in data:
+#                 data['circuits'] = data.pop('circuit')
+            
+#             f = open(dirpath + '/' + filename, 'w')
+#             f.write(newOrginizeJson(data))
+#             f.close()
+            
+
+# 2. change circuit names
 for (dirpath, dirnames, filenames) in os.walk(path):
     
     for filename in filenames:
@@ -20,12 +40,17 @@ for (dirpath, dirnames, filenames) in os.walk(path):
             data = json.loads(json_data)
             
             
-            if 'circuit' in data:
-                data['circuits'] = data.pop('circuit')
+            if data.get('circuits'):
+                for circuit in data.get('circuits'):
+                    if data.get('name') in circuit.get('name'):
+                        circuit['name'] = circuit['name'].split(data['name'])[-1]
+                        for char in ['_','-']:
+                            circuit['name'].replace(char,'')
+                        print(circuit['name'])
+                        # import pdb; pdb.set_trace() 
             
-            f = open(dirpath + '/' + filename, 'w')
-            f.write(newOrginizeJson(data))
-            f.close()
-            
+                f = open(dirpath + '/' + filename, 'w')
+                f.write(newOrginizeJson(data))
+                f.close()
 
 import pdb; pdb.set_trace() 
