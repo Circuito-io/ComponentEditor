376b4f853a118e16ebce12cb444c96788380e40b
eyak <eyal@robo-plan.com>
2019-01-08 01:05:13 +0200

    Add svg data support

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ global.dataFolder = path.join(__dirname , 'components');
 global.circuitoServer = "***REMOVED***
 global.uploadEndpoint = "save_components";
 global.previewEndpoint = "app?u=";
+global.svgdataServer = "***REMOVED***
 
 // Generate unique user uuid
 const uuidv4 = require('uuid/v4');
@@ -22,9 +23,11 @@ app.use(express.static('public'))
 var partsBlocksCodersRoutes = require('./api/routes/partsblockscodersRoutes');
 var previewRoutes = require('./api/routes/previewRoutes');
 var uploadRoutes = require('./api/routes/uploadRoutes');
+var svgdataRoutes = require('./api/routes/svgdataRoutes');
 partsBlocksCodersRoutes(app);
 previewRoutes(app);
 uploadRoutes(app);
+svgdataRoutes(app);
 
 app.listen(port, hostname);
 
