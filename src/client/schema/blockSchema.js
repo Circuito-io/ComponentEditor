553bfccaeaf1223ba2cff6b9ed459c9f5d59d9ec
eyak <eyal@robo-plan.com>
2019-01-22 17:54:31 +0200

    use external schema

--- src/client/schema/blockSchema.js
+++ src/client/schema/blockSchema.js
@@ -1,10 +1,9 @@
+import * as blockSchema from '../../../circuito-schema/block.json';
 import {
   circuitsSchema,
   circuitsuiSchema
 } from "./circuitSchema";
 
-const tagsEnum = ["Controller", "Power supply", "Support"];
-const categoryEnum = ["Controller", "Power supply", "Support"];
 
 export function blockuiSchema(blocksList) {
   return {
@@ -18,7 +17,7 @@ export function blockuiSchema(blocksList) {
     category: {
       "ui:field": "typeahead",
       typeahead: {
-        options: categoryEnum,
+        options: blockSchema.default.properties.app.properties.tags.items.enum,
         minLength: 0,
         multiple: true
       }
@@ -33,7 +32,7 @@ export function blockuiSchema(blocksList) {
       tags: {
         "ui:field": "typeahead",
         typeahead: {
-          options: tagsEnum,
+          options: blockSchema.default.properties.category.items.enum,
           minLength: 0,
           multiple: true
         }
@@ -71,92 +70,3 @@ export function blockuiSchema(blocksList) {
   }
 };
 
-export function blockSchema(blockConnectors) {
-  return {
-    type: "object",
-    required: ["name", "blockId", "category"],
-    properties: {
-      name: {
-        type: "string",
-        title: "Name"
-      },
-      blockId: {
-        type: "string",
-        title: "Block ID"
-      },
-      category: {
-        title: "Category",
-        type: "array",
-        items: {
-          type: "string",
-          enum: categoryEnum
-        },
-        uniqueItems: true
-      },
-      app: {
-        type: "object",
-        title: "Appearance",
-        required: ["appName", "tags", "desc"],
-        properties: {
-          appName: {
-            type: "string",
-            title: "Application name"
-          },
-          numericName: {
-            type: "string",
-            title: "Numeric name"
-          },
-          shortName: {
-            type: "string",
-            title: "Short name"
-          },
-          image: {
-            type: "string",
-            title: "Image"
-          },
-          tags: {
-            title: "Tags",
-            type: "array",
-            items: {
-              type: "string",
-              enum: tagsEnum
-            },
-            uniqueItems: true
-          },
-          visible: {
-            type: "boolean",
-            title: "Visible in application"
-          },
-          indicators: {
-            type: "object",
-            title: "Indicators",
-            properties: {
-              verified: {
-                type: "boolean",
-                default: false
-              },
-              code: {
-                type: "boolean",
-                title: "Has coder support",
-                default: false
-              },
-              solder: {
-                type: "boolean",
-                title: "Requires soldering",
-                default: false
-              }
-            }
-          },
-          desc: {
-            type: "string",
-            title: "Description"
-          }
-        }
-      },
-      notes: {
-        type: "string"
-      },
-      ...circuitsSchema(blockConnectors)
-    }
-  }
-};
