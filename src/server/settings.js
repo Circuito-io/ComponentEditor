b024dc4cd3093708932f3cf40e48d3f258690454
eyak <eyal@robo-plan.com>
2019-01-23 13:19:35 +0200

    overrider circuito server by CIRCUITO_SERVER env variable

--- src/server/settings.js
+++ src/server/settings.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const uuidv4 = require('uuid/v4');
 
 global.dataFolder = path.join(__dirname, '../../components');
-global.circuitoServer = "https://circuito-ci-staging-pr-319.herokuapp.com/";
+global.circuitoServer = process.env.CIRCUITO_SERVER || "https://circuito-ci-staging-pr-319.herokuapp.com/";
 global.uploadEndpoint = "save_components";
 global.previewEndpoint = "app?u=";
 global.svgdataServer = "https://gz1f13eb3m.execute-api.us-west-1.amazonaws.com/Prod/";
