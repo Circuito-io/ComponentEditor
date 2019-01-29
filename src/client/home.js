a964cc98955c34015872746965826f94ebe38621
eyak <eyal@robo-plan.com>
2019-01-29 15:49:13 +0200

    Remove 'getting started' from home screen

--- src/client/home.js
+++ src/client/home.js
@@ -34,25 +34,6 @@ export class Home extends React.Component {
               </Well>
             </Col>
           </Row>
-          
-          
-          <Row>
-            <Col sm={12}>
-              <Well>
-                <h1>Getting Started</h1>
-                These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. See deployment for notes on how to deploy the project on a live system.  
-                <br/>
-                <br/>
-                <Image src="https://res.cloudinary.com/circuito/image/upload/v1545820007/Circuito_4_Entities_1.png" responsive />
-                <br/>
-                <br/>
-                <div className="youtube-container">
-                  <img src="http://placehold.it/16x9"/>
-                  <iframe src="https://www.youtube.com/embed/9YffrCViTVk" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
-                </div>
-              </Well>
-            </Col>
-          </Row>
         </Grid>
     );
   }
