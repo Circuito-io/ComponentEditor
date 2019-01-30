b85ce2a8e5d14361cb69f7dd9e2ff7676f1d33f1
eyak <eyal@robo-plan.com>
2019-01-30 13:58:14 +0200

    Add schema validation across all files, and display errors in client

--- src/client/saveupload.js
+++ src/client/saveupload.js
@@ -1,35 +1,90 @@
 import React from "react";
-import { NavItem, Glyphicon } from "react-bootstrap";
+import { Modal, Button, NavItem, Glyphicon } from "react-bootstrap";
 import { invoke_upload } from "./controller.js";
-import { toast } from 'react-toastify';
+import { toast } from "react-toastify";
 
-export function SaveUpload(props) {
-    return (
-        <NavItem onClick={event => {
-            props.onSave();
+export class SaveUpload extends React.Component {
+  constructor(props) {
+    super(props);
 
-            const toastId = toast.info("Upload in progress...", { autoClose: false });
+    this.state = { show: false, modalMsg: "" };
 
-            invoke_upload().then(response => {
-                if (!response.ok) {
-                    console.log(response);
+    this.onUpload = this.onUpload.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+    this.handleShow = this.handleShow.bind(this);
+  }
 
-                    toast.update(toastId, {
-                        render: 'Upload failed:' + response.statusText,
-                        type: toast.TYPE.ERROR,
-                        autoClose: 5000
-                    });
-                }
-                else {
-                    toast.update(toastId, {
-                        render: "Upload successful",
-                        type: toast.TYPE.SUCCESS,
-                        autoClose: 5000
-                    });
-                }
+  handleClose() {
+    this.setState({ show: false });
+  }
+
+  handleShow() {
+    this.setState({ show: true });
+  }
+
+  onUpload(event) {
+    {
+      this.props.onSave();
+
+      const toastId = toast.info("Upload in progress...", {
+        autoClose: false
+      });
+
+      invoke_upload().then(response => {
+        if (!response.ok) {
+          toast.update(toastId, {
+            render: "Upload failed:" + response.statusText,
+            type: toast.TYPE.ERROR,
+            autoClose: 5000
+          });
+
+          if (response.status === 412) {
+            // Precondition failed - validation errors
+            response.text().then(text => {
+              this.setState({ show: true, modalMsg: text });
             });
+          } else {
+            console.log(response);
+          }
+        } else {
+          toast.update(toastId, {
+            render: "Upload successful",
+            type: toast.TYPE.SUCCESS,
+            autoClose: 5000
+          });
         }
-        }
-        ><Glyphicon glyph="glyphicon-refresh" />Save & Upload</NavItem>
+      });
+    }
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <Modal show={this.state.show} onHide={this.handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Validation errors, upload cancelled</Modal.Title>
+          </Modal.Header>
+          <Modal.Body
+            style={{
+              "max-height": "calc(100vh - 210px)",
+              "overflow-y": "auto"
+            }}
+            dangerouslySetInnerHTML={{__html: this.state.modalMsg}}
+          >
+            
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={this.handleClose}>
+              Close
+            </Button>
+          </Modal.Footer>
+        </Modal>
+
+        <NavItem onClick={this.onUpload}>
+          <Glyphicon glyph="glyphicon-refresh" />
+          Save & Upload
+        </NavItem>
+      </React.Fragment>
     );
+  }
 }
--- src/server/api/controllers/uploadController.js
+++ src/server/api/controllers/uploadController.js
@@ -1,76 +1,127 @@
-'use strict';
+"use strict";
 
-const rp = require('request-promise');
-const fs = require('fs');
-const path = require('path');
+const rp = require("request-promise");
+const fs = require("fs");
+const path = require("path");
 const urlJoin = require("proper-url-join");
+const Validator = require("jsonschema").Validator;
 
-const walkDirSync = (d, res = { Parts: [], Coders: [], Blocks: [], CodeFiles: [] }, type = null, depth = 0) => {
-	var dpath      = path.parse(d)
-	var basename   = dpath.base;
-	var dirname    = dpath.dir;
-	var enddirname = dirname.split(path.sep).pop();
-	var ext        = dpath.ext;
+var partSchema = require("../../../../circuito-schema/part.json");
+var blockSchema = require("../../../../circuito-schema/block.json");
+var coderSchema = require("../../../../circuito-schema/coder.json");
 
-	if (fs.statSync(d).isDirectory()) {
-		// directory
-		if (depth == 1) {
-			if (['Parts', 'Coders', 'Blocks'].includes(basename))
-				type = basename
-			else
-				console.error('Ignoring unkonwn folder', basename);
-		}
+var schema = { type: "number" };
 
-		return fs.readdirSync(d).reduce((res, f) => walkDirSync(path.join(d, f), res, type, depth + 1), res);
-	}
-	else {
-		// file
-		let data = fs.readFileSync(d, 'utf8');
-		if (depth == 2 && ext == '.json') {
-			// json data file
-			data = JSON.parse(data);
-			res[type].push(data);
-		}
-		else if (type == 'Coders' && depth == 3) {
-			// coder file
-			res['CodeFiles'].push({
-				name: basename,
-				coder: enddirname,
-				content: encodeURIComponent(data)
-			});
-		}
-		else {
-			console.error("Ignoring file", d);
-		}
-	}
+const walkDirSync = (
+  d,
+  res = { Parts: [], Coders: [], Blocks: [], CodeFiles: [] },
+  type = null,
+  depth = 0
+) => {
+  var dpath = path.parse(d);
+  var basename = dpath.base;
+  var dirname = dpath.dir;
+  var enddirname = dirname.split(path.sep).pop();
+  var ext = dpath.ext;
 
-	return res;
-}
+  if (fs.statSync(d).isDirectory()) {
+    // directory
+    if (depth == 1) {
+      if (["Parts", "Coders", "Blocks"].includes(basename)) type = basename;
+      else console.error("Ignoring unkonwn folder", basename);
+    }
 
-exports.upload = function (req, res) {
+    return fs
+      .readdirSync(d)
+      .reduce(
+        (res, f) => walkDirSync(path.join(d, f), res, type, depth + 1),
+        res
+      );
+  } else {
+    // file
+    let data = fs.readFileSync(d, "utf8");
+    if (depth == 2 && ext == ".json") {
+      // json data file
+      data = JSON.parse(data);
+      res[type].push(data);
+    } else if (type == "Coders" && depth == 3) {
+      // coder file
+      res["CodeFiles"].push({
+        name: basename,
+        coder: enddirname,
+        content: encodeURIComponent(data)
+      });
+    } else {
+      console.error("Ignoring file", d);
+    }
+  }
 
-	var files = walkDirSync(global.dataFolder);
-	const endPoint = urlJoin(global.circuitoServer, global.uploadEndpoint, global.userid, { trailingSlash: true });
+  return res;
+};
 
-	console.log("Sending to", endPoint)
+exports.upload = function(req, res) {
+  var files = walkDirSync(global.dataFolder);
 
-	global.analytics.track({
-		userId: global.userid,
-		event: 'Data Uploaded'
-	});
+  var validator = new Validator();
+  var errorString = "";
 
-	rp({
-		url: endPoint,
-		method: 'post',
-		body: files,
-		json: true,
-	})
-		.then(body => {
-			console.log("Upload OK");
-			res.sendStatus(200);
-		})
-		.catch(err => {
-			console.log(err)
-			res.status(404).send(err.message);
-		})
+  const schemaSets = [
+	{ type: "Blocks", schema: blockSchema },
+    { type: "Parts", schema: partSchema },
+    { type: "Coders", schema: coderSchema }
+  ];
+
+  schemaSets.map(schemaSet => {
+    files[schemaSet.type].map(obj => {
+      var validation = validator.validate(obj, schemaSet.schema);
+      if (validation.errors.length > 0) {
+        errorString += `<h4>${schemaSet.type}:${
+          obj.name
+        }</h4>\n</br><ul>${validation.errors
+          .map(error => `<li>${error}</li>`)
+          .join("\n")}</ul> \n\n</br></br>`;
+      }
+    });
+  });
+
+  if (errorString) {
+    global.analytics.track({
+      userId: global.userid,
+      event: "Schema Failed"
+    });
+
+    console.log("Found schema errors, aborting");
+    console.log("Found schema errors, aborting");
+    res.status(412).send(errorString);
+    return;
+  }
+
+  const endPoint = urlJoin(
+    global.circuitoServer,
+    global.uploadEndpoint,
+    global.userid,
+    { trailingSlash: true }
+  );
+
+  console.log("Sending to", endPoint);
+
+  global.analytics.track({
+    userId: global.userid,
+    event: "Data Uploaded"
+  });
+
+  rp({
+    url: endPoint,
+    method: "post",
+    body: files,
+    json: true
+  })
+    .then(body => {
+      console.log("Upload OK");
+      res.sendStatus(200);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(404).send(err.message);
+    });
 };
