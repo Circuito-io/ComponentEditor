162904fc292e8b4b283d2522b0faed38ae9c60be
eyak <eyal@robo-plan.com>
2019-02-15 11:56:05 +0200

    make gitpod use prebuilt production, improve packages.json

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,52 @@
 const path = require("path");
 const webpack = require("webpack");
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const NodemonPlugin = require( 'nodemon-webpack-plugin' )
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const NodemonPlugin = require("nodemon-webpack-plugin");
 
-const outputDirectory = 'dist';
+const outputDirectory = "dist";
+const publicDirectory = "public";
 
-module.exports = {
-  entry: './src/client/app.js',
-  output: {
-    path: path.join(__dirname, outputDirectory),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [{
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
+module.exports = env => {
+  return {
+    entry: "./src/client/app.js",
+    output:
+      env === "prod"
+        ? {
+            // both filed served as static, outputDirectory is resolved first in server/index.js
+            path: path.join(__dirname, publicDirectory),
+            filename: "bundle.js"
+          }
+        : {
+            path: path.join(__dirname, outputDirectory),
+            filename: "bundle.js"
+          },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader"
+          }
+        },
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"]
+        },
+        {
+          test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+          loader: "url-loader?limit=100000"
         }
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      },
-      {
-        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
-        loader: 'url-loader?limit=100000'
-      }
+      ]
+    },
+    plugins: [
+      new CleanWebpackPlugin([outputDirectory]),
+      new NodemonPlugin({
+        watch: ["./src", "./dist"],
+        script: "./src/server/index.js",
+        verbose: true,
+        ignore: ["*.js.map"]
+      })
     ]
-  },
-  plugins: [
-    new CleanWebpackPlugin([outputDirectory]),
-    new NodemonPlugin({
-      watch: ['./src', './dist'],
-      script: './src/server/index.js',
-      verbose: true,
-      ignore: ['*.js.map'],
-    })
-  ]
+  };
 };
