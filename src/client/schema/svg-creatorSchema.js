39bd9da49b5c9db888024a2745c83b0d6bd987c1
eyak <eyal@robo-plan.com>
2019-02-17 14:39:04 +0200

    svg-creator bug

--- src/client/schema/svg-creatorSchema.js
+++ src/client/schema/svg-creatorSchema.js
@@ -1,58 +1,55 @@
-export const svguiSchema = {
-
-};
+export const svguiSchema = {};
 
 export const svgSchema = {
-    type: "object",
-    properties: {
-        name: {
+  type: "object",
+  required: ["width", "height", "color"],
+  properties: {
+    name: {
+      type: "string",
+      title: "Name"
+    },
+    width: {
+      title: "Width [mm]",
+      default: 0,
+      type: "number"
+    },
+    height: {
+      title: "Height [mm]",
+      default: 0,
+      type: "number"
+    },
+    color: {
+      title: "Color",
+      type: "string",
+      default: "Red",
+      enum: ["#CF2F27", "#45925A", "#0B5597", "#000000", "#338085"],
+      enumNames: ["Red", "Green", "Blue", "Black", "Ocean"]
+    },
+    pinTypes: {
+      title: "Pin Type",
+      type: "string",
+      default: "pads",
+      enum: ["pads", "headers"]
+    },
+    pins: {
+      type: "array",
+      title: "Pins:",
+      items: {
+        type: "object",
+        required: ["name", "type"],
+        properties: {
+          name: {
+            title: "Pin Name:",
+            type: "string"
+          },
+          type: {
+            title: "Pin Type:",
             type: "string",
-            title: "Name"
-        },
-        width: {
-            title: "Width [mm]",
-            type: "integer"
-        },
-        height: {
-            title: "Height [mm]",
-            type: "integer"
-        },
-        color: {
-            title: "Color",
-            type: "string",
-            default: "Red",
-            enum: ['#CF2F27','#45925A', '#0B5597', '#000000','#338085' ],
-            enumNames: ['Red','Green', 'Blue','Black', 'Ocean']
-        },
-        pinTypes: {
-            title: "Pin Type",
-            type: "string",
-            default: "pads",
-            enum: ["pads", "headers"]
-        },
-        pins: {
-            type: "array",
-            title: "Pins:",
-            items: {
-                type: "object",
-                required: [
-                "name",
-                "type"
-                ],
-                properties: {
-                    name: {
-                        title: "Pin Name:",
-                        type: "string"
-                    },
-                    type: {
-                        title: "Pin Type:",
-                        type: "string",
-                        default: "male",
-                        enum: ["male", "female"]
-                    }
-                }
-            }
+            default: "male",
+            enum: ["male", "female"]
+          }
         }
-
+      }
     }
+  }
 };
