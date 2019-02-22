1bf42910a10cd0cef6c79d69593f6a5cf93b33ff
eyak <eyal@robo-plan.com>
2019-02-22 10:50:30 +0200

    added how this works

--- src/client/home.js
+++ src/client/home.js
@@ -18,14 +18,39 @@ export class Home extends React.Component {
       <Container>
         <Row>
           <Col>
-            <Card body>
-              <h2>Edit or create a block</h2>
-              <br />
-              <BlocksList
-                cachedData={this.props.cachedData}
-                onBlockSelected={this.onBlockSelected}
-                refreshData={this.props.refreshData}
-              />
+            <Card>
+              <Card.Header>
+                <h3>Edit or Create a Block</h3>
+              </Card.Header>
+              <Card.Body style={{ padding: "2rem 0.5rem" }}>
+                <BlocksList
+                  cachedData={this.props.cachedData}
+                  onBlockSelected={this.onBlockSelected}
+                  refreshData={this.props.refreshData}
+                />
+              </Card.Body>
+            </Card>
+            <Card>
+              <Card.Header>
+                <h3>How this works</h3>
+              </Card.Header>
+              <Card.Body>
+                <ol>
+                  <li>
+                    Choose a block to <b>edit</b> or create a new one
+                  </li>
+                  <li>
+                    Modify and <b>save</b> it
+                  </li>
+                  <li>
+                    Click <b>'Preview'</b> to see it in a circuito sandbox
+                  </li>
+                  <li>
+                    <b>Publish (pull-request)</b> your work so other can enjoy
+                    it
+                  </li>
+                </ol>
+              </Card.Body>
             </Card>
           </Col>
         </Row>
